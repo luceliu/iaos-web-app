46e97e9c8483f2e27dca79f0313114b9774ba116
Luce <lucy_liu@live.ca>
2019-04-15 01:44:30 +0800

    added Browser Navigation thanks to https://korban.net/posts/elm/2018-10-12-elm-guide-missing-part-url-handling-browser.application/

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,17 @@
 module Main exposing (main)
 
-import Browser
-import Css exposing (..)
-import Html
-import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (css, href, src)
+import Browser exposing (Document, UrlRequest(..))
+import Css as CSS exposing (..)
+import Html exposing (..)
+import Html.Attributes as HA exposing (..)
+--import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (..)
 import Html.Styled.Events exposing (onClick)
 import Styles exposing (..)
 import Elements exposing (..)
-
-
+import Browser.Navigation as Nav
+import Url exposing (Url)
+import Url.Parser as UrlParser exposing ((</>))
 
 
 {-| A plain old record holding a couple of theme colors.
@@ -29,51 +31,114 @@ mycolors =
 -}
 legacyBorderRadius : String -> Style
 legacyBorderRadius amount =
-    Css.batch
-        [ property "-moz-border-radius" amount
-        , property "-webkit-border-top-left-radius" amount
-        , property "-webkit-border-top-right-radius" amount
-        , property "-webkit-border-bottom-right-radius" amount
-        , property "-webkit-border-bottom-left-radius" amount
-        , property "border-radius" amount
+    CSS.batch
+        [ CSS.property "-moz-border-radius" amount
+        , CSS.property "-webkit-border-top-left-radius" amount
+        , CSS.property "-webkit-border-top-right-radius" amount
+        , CSS.property "-webkit-border-bottom-right-radius" amount
+        , CSS.property "-webkit-border-bottom-left-radius" amount
+        , CSS.property "border-radius" amount
         ]
 
 {- Actual HTML stuff:
 -}
 
 
-view : Model -> Html Msg
+view : Model -> Document Msg -- document?
+--view model =
+--    div []
+--        [ headroom
+--        , twoColumns Elements.logo (text "Smol cute pics of animals or whatever")
+--        , upcomingEvents
+--        , blogPosts
+--        , text lorem
+--        ]
+
 view model =
-    div []
-        [ headroom
-        , twoColumns Elements.logo (text "Smol cute pics of animals or whatever") 
-        , upcomingEvents
-        , blogPosts
-        , text lorem 
+    let
+        inline =
+            HA.style "display" "inline-block"
+
+        padded =
+            HA.style "padding" "10px"
+
+        menu =
+            div [ HA.style "padding" "10px", HA.style "border-bottom" "1px solid #c0c0c0" ]
+                [ a [ inline, padded, HA.href "/Basics" ] [ text "Basics" ]
+                , a [ inline, padded, HA.href "/Maybe" ] [ text "Maybe" ]
+                , a [ inline, padded, HA.href "/List" ] [ text "List" ]
+                ]
+
+        title =
+            case model.route of
+                Just route ->
+                    Tuple.first route
+                        ++ (case Tuple.second route of
+                                Just function ->
+                                    "." ++ function
+
+                                Nothing ->
+                                    ""
+                           )
+
+                Nothing ->
+                    "Invalid route"
+    in
+    { title = "URL handling example"
+    , body =
+        [ menu
+        , h2 [] [ text title ]
         ]
+    }
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
-        { view = view >> toUnstyled
+    Browser.application
+        { init = init
         , update = update
-        , init = initialModel
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        , onUrlRequest = ClickLink
+        , onUrlChange = ChangeUrl
         }
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg)
 update msg model =
-    model
+    case msg of
+        ChangeUrl url ->
+            ( { model | route = UrlParser.parse docsParser url }, Cmd.none )
+
+        ClickLink urlRequest ->
+            case urlRequest of
+                Internal url ->
+                    ( model, Nav.pushUrl model.navKey <| Url.toString url )
+
+                External url ->
+                    ( model, Nav.load url )
 
 
 type Msg
-    = DoSomething
+    = ChangeUrl Url
+    | ClickLink UrlRequest
 
+type alias DocsRoute =
+    ( String, Maybe String )
 
 type alias Model =
-    ()
+    { navKey : Nav.Key
+    , route : Maybe DocsRoute
+    }
+
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url navKey =
+    ( { navKey = navKey, route = UrlParser.parse docsParser url }, Cmd.none )
+
+docsParser : UrlParser.Parser (DocsRoute -> a) a
+docsParser =
+    UrlParser.map Tuple.pair (UrlParser.string </> UrlParser.fragment identity)
 
 
-initialModel : Model
-initialModel =
-    ()
+--initialModel : Model
+--initialModel =
+--    ()
